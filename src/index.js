752e022cb1bf696e07444558340e7316e6dfc8ee
ravikanth34 <ravikanth34@gmail.com>
2025-12-05 15:54:07 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme/theme';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </Provider>
+);
